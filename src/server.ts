634b29e80daf02867bc185af0e981f05a2bd2e3d
Viniciuslb067 <viniciuslb067@gmail.com>
2022-04-18 00:04:11 -0300

    use envoriment variable on server port

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,15 @@ import express from 'express';
 
 import { router } from "./routes";
 
+const port = process.env.API_PORT || 3008;
+
 const app = express();
 
 app.use(express.json());
 app.use(router);
 
-app.listen(3008, () => {
-  console.log('Server is running on port 3008');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
   cron.schedule('* * * * *', async () => {
     console.log('cron job running');
   });
