01935c42227def71fa978d6f8f3815d0f56f91a7
Viniciuslb067 <viniciuslb067@gmail.com>
2022-04-18 00:11:20 -0300

    try fix heroku error deploy

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,13 @@ import express from 'express';
 
 import { router } from "./routes";
 
-const port = process.env.API_PORT || 3008;
-
 const app = express();
 
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.API_PORT || 3008, () => {
+  console.log(`Server is running on port ${process.env.API_PORT || 3008}`);
   cron.schedule('* * * * *', async () => {
     console.log('cron job running');
   });
