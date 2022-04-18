f0786b2a962cb2aa911fcbf9fd4de27396be8309
Viniciuslb067 <viniciuslb067@gmail.com>
2022-04-18 12:00:22 -0300

    add cors for all origins

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import cron from "node-cron";
 import express from 'express';
+import cors from "cors";
 
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(process.env.PORT || 3008, () => {
