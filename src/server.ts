b5ce5a2b15331220fbb5d1ef330af81166443eb4
Viniciuslb067 <viniciuslb067@gmail.com>
2022-04-18 00:39:21 -0300

    try fix heroku error on port

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.API_PORT || 3008, () => {
-  console.log(`Server is running on port ${process.env.API_PORT || 3008}`);
+app.listen(process.env.PORT || 3008, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3008}`);
   cron.schedule('* * * * *', async () => {
     console.log('cron job running');
   });
